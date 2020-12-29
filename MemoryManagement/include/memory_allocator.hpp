b90631fc8d7b38512e7a2e695668feb7a4cab2e7
xmmt <mkmax472@gmail.com>
2020-12-29 05:09:56 +0300

    implementing fixed size allocator

--- MemoryManagement/include/memory_allocator.hpp
+++ MemoryManagement/include/memory_allocator.hpp
@@ -1,6 +1,10 @@
 #pragma once
-
+#define _ENABLE_EXTENDED_ALIGNED_STORAGE
+#include <cassert>
 #include <memory>
+#include <iostream>
+#include <string>
+#include <cctype>
 
 namespace memory {
 
@@ -17,31 +21,58 @@ public:
 #endif
 };
 
-template <size_t BlockSize, size_t Alignment, size_t BlockCount>
+template <size_t BlockSize, size_t BlockCount>
 class FixedSizeAllocator final : public AllocatorInterface {
 public:
-    FixedSizeAllocator() {
-    }
-    FixedSizeAllocator(FixedSizeAllocator const& other) {
-    }
-    FixedSizeAllocator(FixedSizeAllocator&& other) {
-    }
-    FixedSizeAllocator& operator=(FixedSizeAllocator const& other) {
-        return *this;
-    }
-    FixedSizeAllocator& operator=(FixedSizeAllocator&& other) {
-        return *this;
-    }
+    FixedSizeAllocator() = default;
+    FixedSizeAllocator(FixedSizeAllocator const&) = delete;
+    FixedSizeAllocator(FixedSizeAllocator&&) = default;
+    FixedSizeAllocator& operator=(FixedSizeAllocator const&) = delete;
+    FixedSizeAllocator& operator=(FixedSizeAllocator&&) = default;
     ~FixedSizeAllocator() final {
     }
 
     void init() final {
+        buffer_ = std::make_unique<buffer_storage_t>();
+        start_bound_pointer_ = static_cast<void*>(buffer_.get());
+        end_bound_pointer_ = static_cast<void*>(std::next(buffer_.get()));
+        auto* first_block_ptr = static_cast<block*>(start_bound_pointer_);
+        new (first_block_ptr) block;
+        *static_cast<block**>(static_cast<void*>(&first_block_ptr->storage)) = nullptr;
+        init_blocks_count_ = 1;
+        free_block_ptr_ = first_block_ptr;
+        auto* p = static_cast<block*>(static_cast<void*>(buffer_.get()));
+
+        //for (int i = 0; i < BlockCount; ++i, std::advance(p, 1)) {
+        //    new (p) block;
+        //    static_cast<std::byte*>(static_cast<void*>(&p->storage))[0] = static_cast<std::byte>(i);
+        //    static_cast<std::byte*>(static_cast<void*>(&p->storage))[BlockSize - 1] = static_cast<std::byte>(24);
+        //}
     }
     void destroy() final {
     }
     void* alloc(size_t size) final {
+        if (init_blocks_count_ < BlockCount) {
+            auto* new_block_ptr = std::next(static_cast<block*>(start_bound_pointer_), init_blocks_count_);
+            new (new_block_ptr) block;
+            *static_cast<block**>(static_cast<void*>(&new_block_ptr->storage)) = free_block_ptr_;
+            free_block_ptr_ = new_block_ptr;
+            init_blocks_count_ += 1;
+        }
+        assert(check_free(free_block_ptr_));
+        auto* block_ptr = free_block_ptr_;
+        free_block_ptr_ = *static_cast<block**>(static_cast<void*>(&block_ptr->storage));
+#if _DEBUG
+        block_ptr->occupied = true;
+#endif
+        return &block_ptr->storage;
     }
     void free(void* p) final {
+        auto* block_ptr = static_cast<block*>(static_cast<void*>(std::prev(static_cast<std::byte*>(p), offsetof(block, storage))));
+        assert(check_occupied(block_ptr));
+#if _DEBUG
+        block_ptr->occupied = false;
+#endif
     }
 #if _DEBUG
     void dumpStat() const final {
@@ -49,42 +80,95 @@ public:
     void dumpBlocks() const final {
     }
 #endif
+    void dumpBuffer() const {
+        std::cout.setf(std::ios::right, std::ios::adjustfield);
+        auto* p = static_cast<uint8_t*>(static_cast<void*>(buffer_.get()));
+        for (int i = 0; i < sizeof(buffer_storage_t); ++i, ++p) {
+            std::cout.width(0);
+            std::cout << static_cast<void*>(p);
+            std::cout.width(7);
+            std::cout << "[" + std::to_string(i) + "]";
+            std::cout.width(5);
+            std::cout << static_cast<int>(*p);
+            if (std::isgraph(*p)) {
+                std::cout.width(5);
+                std::cout << *p;
+            }
+            std::cout << std::endl;
+        }
+    }
+
 private:
-    using block_storage_t = std::aligned_storage_t<BlockSize, Alignment>;
-    static constexpr size_t start_tag_value{ 123456789 };
-    static constexpr size_t end_tag_value{ 987654321 };
-    struct service_info {
-    };
+    // block structure
+    static constexpr size_t block_alignment{ 8 };
+    using block_storage_t = std::aligned_storage_t<BlockSize, block_alignment>;
+#if _DEBUG
+    static constexpr uint64_t start_tag_value{ 0x73'74'61'72'74'74'61'67ull }; // "starttag"
+    static constexpr uint64_t end_tag_value{ 0x65'6e'64'5f'5f'74'61'67ull };   // "end__tag"
+#endif
+    using block_offset_t = size_t;
     struct block {
-        service_info info;
 #if _DEBUG
-        size_t start_tag{ start_tag_value };
+        bool occupied{ false };
+        uint64_t start_tag{ start_tag_value };
 #endif
-        block_storage_t storage;
+        block_storage_t storage{};
 #if _DEBUG
-        size_t end_tag{ end_tag_value };
+        uint64_t end_tag{ end_tag_value };
 #endif
     };
+    static constexpr auto block_size = sizeof(block);
+
+    // buffer storage
     using buffer_storage_t = std::aligned_storage_t<BlockCount * sizeof(block), alignof(block)>;
     std::unique_ptr<buffer_storage_t> buffer_{ nullptr };
+    void* start_bound_pointer_{ nullptr };
+    void* end_bound_pointer_{ nullptr };
+    block* free_block_ptr_{ nullptr };
+    size_t init_blocks_count_{ 0 };
+    size_t occupied_blocks_count_{ 0 };
+
+private:
+#if _DEBUG
+    bool check_boundary(block* block_ptr) const {
+        if (std::distance(static_cast<std::byte*>(start_bound_pointer_), static_cast<std::byte*>(static_cast<void*>(block_ptr))) < 0
+          || std::distance(static_cast<std::byte*>(static_cast<void*>(block_ptr)), static_cast<std::byte*>(end_bound_pointer_)) <= 0) {
+            return false;
+        }
+        if (reinterpret_cast<uint64_t>(block_ptr) % block_size != 0) {
+            return false;
+        }
+        return true;
+    }
+    static bool check_corruption(block* block_ptr) {
+        return block_ptr->start_tag == start_tag_value && block_ptr->end_tag == end_tag_value;
+    }
+    bool check_occupied(block* block_ptr) const {
+        if (!check_boundary(block_ptr)) {
+            return false;
+        }
+        return block_ptr->occupied;
+    }
+    bool check_free(block* block_ptr) const {
+        if (!check_boundary(block_ptr)) {
+            return false;
+        }
+        if (!check_corruption(block_ptr)) {
+            return false;
+        }
+        return !block_ptr->occupied;
+    }
+#endif
 };
 
 class MemoryAllocator final : public AllocatorInterface {
 public:
-    MemoryAllocator() {
-    }
-    MemoryAllocator(MemoryAllocator const& other) {
-    }
-    MemoryAllocator(MemoryAllocator&& other) {
-    }
-    MemoryAllocator& operator=(MemoryAllocator const& other) {
-        return *this;
-    }
-    MemoryAllocator& operator=(MemoryAllocator&& other) {
-        return *this;
-    }
-    ~MemoryAllocator() final {
-    }
+    MemoryAllocator() = default;
+    MemoryAllocator(MemoryAllocator const& other) = delete;
+    MemoryAllocator(MemoryAllocator&& other) = default;
+    MemoryAllocator& operator=(MemoryAllocator const& other) = delete;
+    MemoryAllocator& operator=(MemoryAllocator&& other) = default;
+    ~MemoryAllocator() final = default;
     void init() final {
     }
     void destroy() final {
--- MemoryManagement/source/source.cpp
+++ MemoryManagement/source/source.cpp
@@ -2,6 +2,8 @@
 
 int main() {
     memory::MemoryAllocator ma;
-
+    memory::FixedSizeAllocator<16, 16> fsa;
+    fsa.init();
+    fsa.dumpBuffer();
     return 0;
 }
